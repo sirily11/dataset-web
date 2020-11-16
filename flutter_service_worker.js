85a7bf5d015f3ba1d29fcdf8c9a0a5aafa32abf0
sirily11 <sirily11@users.noreply.github.com>
2020-11-16 12:44:30 +0000

    deploy: 499375f4cdf77031152d888749d3cf64b849434a

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -12,8 +12,8 @@ const RESOURCES = {
 "manifest.json": "830979572189804ec84340b2a43fd705",
 "main.dart.js": "8cc76b6468e923c0d0e9481850c3073c",
 "version.json": "05cc3f5cf759881834ffd00d95c20bc4",
-"index.html": "5b32df73bbe6ba5e756b3bb8514fcb9f",
-"/": "5b32df73bbe6ba5e756b3bb8514fcb9f",
+"index.html": "62fcdb01b1b63f5cabd8e81dce400f88",
+"/": "62fcdb01b1b63f5cabd8e81dce400f88",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1"
 };
