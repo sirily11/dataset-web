70d974d3b4db643f3ffee8cbbfa2e629533ab68e
sirily11 <sirily1997@gmail.com>
2021-06-06 02:31:33 +0800

    start react app

--- lib/utils/urls.dart
+++
@@ -1,4 +0,0 @@
-// const kBaseURL = "http://0.0.0.0:8000";
-const kBaseURL = "https://eucnu0fqml.execute-api.us-east-1.amazonaws.com/dev";
-const kSinaURL = "/api/sina/keyword";
-const kDisneyShanghaiURL = "/api/disney/facility";
